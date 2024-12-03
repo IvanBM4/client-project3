4c854b59aa3c3a9684daecff155a348fa029e27d
Mercedes <mrcdssebastian@gmail.com>
2024-12-03 18:00:42 +0100

    new pages

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,37 +1,43 @@
 import React from "react";
-import './AboutUsPage.css'
+import { Container, Row, Col } from "react-bootstrap";
+import './AboutUsPage.css';
 
 
 const AboutUsPage = () => {
     return (
-        <div className="about-us-container">
-            <header className="about-us-header">
+        <Container className="mt-5">
+            <header className="about-us-header text-center mb-4">
                 <h1>Sobre Nosotros</h1>
                 <p>Conoce más sobre nuestra misión y compromisos.</p>
             </header>
             <section className="about-us-content">
-                <h2>Nuestra Misión</h2>
-                <p>
-                    Nuestra misión es brindar planes y actividades a tus dias aburridos.
-                </p>
-
-                <h2>Nuestra Visión</h2>
-                <p>
-                    somos lo que se le llama lideres en la insudtria del conectar con gente .
-                </p>
+                <Row className="mb-4">
+                    <Col md={6}>
+                        <h2>Nuestra Misión</h2>
+                        <p>
+                            Nuestra misión es brindar planes y actividades a tus días aburridos.
+                        </p>
+                    </Col>
+                    <Col md={6}>
+                        <h2>Nuestra vision</h2>
+                        <p>
+                            Somos lo que se le llama líderes en la industria de conectar con gente.
+                        </p>
+                    </Col>
+                </Row>
 
                 <h2>Valores</h2>
                 <ul>
                     <li>Integridad</li>
-                    <li>Innovacion</li>
+                    <li>Innovación</li>
                     <li>Compromiso con el cliente</li>
                     <li>Sostenibilidad</li>
                     <li>Sinceridad</li>
-                    <li>planes innovadores</li>
+                    <li>Planes innovadores</li>
                 </ul>
             </section>
-        </div>
+        </Container>
     );
-};
+}
 
 export default AboutUsPage;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,11 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "../pages/HomePage/HomePage"
 import ActivitiesPage from "../pages/ActivitiesPage/ActivitiesPage"
 import AboutUsPage from "../pages/AboutUsPage/AboutUsPage"
+import ActivityDetailsPage from "../pages/ActivityDetailsPage/ActivityDetailsPage"
+import LoginPage from "../pages/LoginPage/LoginPage";
+import SignUpPage from "../pages/SignUpPage/SignUpPage"
+
+
 
 
 const AppRoutes = () => {
@@ -11,6 +16,10 @@ const AppRoutes = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/planes" element={<ActivitiesPage />} />
             <Route path="/sobre-nosotros" element={<AboutUsPage />} />
+            <Route path="/planes/detalles/:id" element={<ActivityDetailsPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/Sign-up" element={<SignUpPage />} />
+
         </Routes>
     )
 }
