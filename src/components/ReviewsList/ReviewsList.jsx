337c024d2d185861c8725b26323fb96a8c329cbb
Mercedes <mrcdssebastian@gmail.com>
2024-12-04 11:09:54 +0100

    changed

---
+++ src/components/ReviewsList/ReviewsList.jsx
@@ -0,0 +1,17 @@
+import ReviewsCard from '../ReviewsCard/ReviewsCard.jsx'
+import { Row, Col } from 'react-bootstrap'
+
+const ReviewsList = ({ reviews }) => {
+    return (
+        <div className="ReviewsList">
+            <h1>Cards</h1>
+        </div>
+    )
+}
+
+export default ReviewsList
+
+
+
+
+
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,6 +5,9 @@ import AboutUsPage from "../pages/AboutUsPage/AboutUsPage"
 import ActivityDetailsPage from "../pages/ActivityDetailsPage/ActivityDetailsPage"
 import LoginPage from "../pages/LoginPage/LoginPage";
 import SignUpPage from "../pages/SignUpPage/SignUpPage"
+import NotFound from "../pages/NotFound/NotFound"
+import ReviewsCard from "../components/ReviewsCard/ReviewsCard"
+import ReviewsList from "../components/ReviewsList/ReviewsList"
 
 
 
@@ -18,7 +21,11 @@ const AppRoutes = () => {
             <Route path="/sobre-nosotros" element={<AboutUsPage />} />
             <Route path="/planes/detalles/:id" element={<ActivityDetailsPage />} />
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/Sign-up" element={<SignUpPage />} />
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="/reviews" element={<ReviewsCard />} />
+            <Route path="/reviewslist" element={<ReviewsList />} />
+
 
         </Routes>
     )
