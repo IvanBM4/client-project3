3aa768b827972b22521d0a79bbb87d358b8b8734
Ivan Blanco <ivanblanco4m@gmail.com>
2024-12-04 14:19:51 +0100

    refactor

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,33 +6,35 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function Navigation() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container fluid>
-                <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Form className="d-flex me-auto">
-                        <Form.Control
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant="dark">Search</Button>
-                    </Form>
-                    <Nav
-                        className="ms-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <Nav.Link href="#action1">Planes</Nav.Link>
-                        <Nav.Link href="#action2">Crea tu plan</Nav.Link>
-                        <Nav.Link href="#action3" disabled>Iniciar Sesión</Nav.Link>
-                        <Nav.Link href="#action4" disabled>Regístrate</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <div className="Navigation">
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container fluid>
+                    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Form className="d-flex me-auto">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="dark">Search</Button>
+                        </Form>
+                        <Nav
+                            className="ms-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link href="#action1">Planes</Nav.Link>
+                            <Nav.Link href="#action2">Crea tu plan</Nav.Link>
+                            <Nav.Link href="#action3" disabled>Iniciar Sesión</Nav.Link>
+                            <Nav.Link href="#action4" disabled>Regístrate</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
     );
 }
 
--- src/pages/NewActivityPage/NewActivityPage.jsx
+++ src/pages/NewActivityPage/NewActivityPage.jsx
@@ -3,14 +3,12 @@ import "./NewActivityPage.css"
 
 const NewActivityPage = () => {
 
-
     return (
-        <Container>
-            <h1>TITULO PARA VER SI FUNCIONA</h1>
-
-
-
-        </Container>
+        <div className="NewActivityPage">
+            <Container>
+                <h1>TITULO PARA VER SI FUNCIONA</h1>
+            </Container>
+        </div>
     )
 }
 export default NewActivityPage
