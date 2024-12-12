76c0cadaa3877c9042793e1793f690dae06298d6
Mercedes <mrcdssebastian@gmail.com>
2024-12-12 08:02:42 +0100

    solved

--- src/components/BouncingLogo/BouncingLogo.jsx
+++ src/components/BouncingLogo/BouncingLogo.jsx
@@ -2,13 +2,27 @@ import React from 'react'
 import './BouncingLogo.css'
 
 const BouncingLogo = () => {
-    const letters = "PLANNING TO GO"
+    const letters = "Planning To Go ".split('')
+    const colors = [
+        '#FF5733', '#33FF57', '#3357FF', '#FF33A1',
+        '#FFC300', '#DAF7A6', '#581845', '#900C3F',
+        '#C70039', '#FFC300', '#FF5733', '#33FF57',
+        '#3357FF', '#FF33A1'
+    ]
 
     return (
-        <div className="bounce">
-            {letters.split('').map((letter, index) => (
-                <span key={index} className="letter">
+        <div className="bouncing-logo">
+            {letters.map((letter, index) => (
+                <span
+                    key={index}
+                    className="bouncing-letter"
+                    style={{
+                        animationDelay: `${index * 0.1}s`,
+                        color: letter === ' ' ? 'transparent' : colors[index % colors.length]
+                    }}
+                >
                     {letter}
+                    <span className="letter-shadow"></span>
                 </span>
             ))}
         </div>
